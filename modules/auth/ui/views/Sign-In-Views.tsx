249c7a59ffd51f9049722b22cc9c8418d3aed0f6
Asif Uddin Ahmed <mylifeasasif@gmail.com>
2026-01-31 21:08:47 +0100

    dashboard sidebar

--- modules/auth/ui/views/Sign-In-Views.tsx
+++ modules/auth/ui/views/Sign-In-Views.tsx
@@ -192,7 +192,7 @@ export const SignInView = () => {
                     </Form>
 
 
-                    <div className="bg-radial from-green-700 to-green-900 relative hidden md:flex flex-col gap-y-4 items-center justify-center">
+                    <div className="bg-radial from-sidebar-accent to-sidebar relative hidden md:flex flex-col gap-y-4 items-center justify-center">
                         <Image src="/logo.svg" alt="Image" height={92} width={92} />
                         <p className="text-2xl font-semibold text-white">
                             SynapSe
--- modules/auth/ui/views/Sign-Up-Views.tsx
+++ modules/auth/ui/views/Sign-Up-Views.tsx
@@ -94,7 +94,7 @@ export const SignUpView = () => {
         <div className="flex flex-col gap-6">
             <Card className="overflow-hidden p-0">
                 <CardContent className="grid p-0 md:grid-cols-2">
-                    <div className="bg-radial from-green-700 to-green-900 relative hidden md:flex flex-col gap-y-4 items-center justify-center">
+                    <div className="bg-radial from-sidebar-accent to-sidebar relative hidden md:flex flex-col gap-y-4 items-center justify-center">
                         <Image src="/logo.svg" alt="Image" height={92} width={92} />
                         <p className="text-2xl font-semibold text-white">
                             SynapSe
