70a9cbd4c8efadeea76e90217eba8b316e58a3c2
Asif Uddin Ahmed <mylifeasasif@gmail.com>
2026-02-01 15:10:06 +0100

    tRPC setup

--- modules/home/ui/views/Home-Views.tsx
+++ modules/home/ui/views/Home-Views.tsx
@@ -1,20 +1,16 @@
 "use client"
 
-import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+
 
 export const HomeView = () => {
-  const {data : session} = authClient.useSession();
+  const trpc = useTRPC();
+  const { data } = useQuery(trpc.hello.queryOptions({text: "Asif"}))
 
-  if(!session){
-    return (
-      <p>Loading...</p>
-    )
-  }
   return (
     <div>
-      <p>{session.user.name}</p>
-      <Button onClick={()=>authClient.signOut()}>Sign Out</Button>
+      {data?.greeting}
     </div>
   );
 }
